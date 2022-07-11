c34d74273e20c814a6cc305ca3d27c2885ec8f87
paulwatconed <paulw@coned.com>
2022-07-11 03:19:11 -0400

    Add project files.

---
+++ RedisConnectionTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RedisConnectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+           // Console.WriteLine("Saving random data in cache");
+            //program.SaveBigData();
+
+            Console.WriteLine("Reading data from cache");
+            program.ReadData();
+
+            Console.ReadLine();
+        }
+
+        public void ReadData()
+        {
+            var cache = RedisConnectorHelper.Connection.GetDatabase();
+            var devicesCount = 10000; // 10000;
+            for (int i = 0; i < devicesCount; i++)
+            {
+                var value = cache.StringGet($"Device_Status:{i}");
+                Console.WriteLine($"Valor={value}");
+            }
+        }
+
+        //public void SaveBigData()proxy
+
+        //{
+        //    var devicesCount = 10000;
+        //    var rnd = new Random();
+        //    var cache = RedisConnectorHelper.Connection.GetDatabase();
+
+        //    for (int i = 1; i < devicesCount; i++)
+        //    {
+        //        var value = rnd.Next(0, 10000);
+        //        cache.StringSet($"Device_Status:{i}", value);
+        //    }
+        //}
+    }
+}
---
+++ RedisConnectionTest/RedisConnectorHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisConnectionTest
+{
+    class RedisConnectorHelper
+    {
+        static RedisConnectorHelper()
+        {
+            RedisConnectorHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("Coned-Dev-Redis-POC.redis.cache.windows.net:6380,password=6HH2iQjEIzEGa3Tgl1q5whpJ2WIsPLnMdAzCaN6FbP4=,ssl=True,abortConnect=False");  //localhost
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
